7581378f6003bfeef1a29dce607d12f1d32562fa
Moataz Bahaa <86017015+moataz-bahaa@users.noreply.github.com>
2024-05-20 12:33:32 +0300

    minor chage

--- features/employee/employee.service.js
+++ features/employee/employee.service.js
@@ -1,14 +1,10 @@
 export const formateEmployee = (empoloyee) => {
   const team = empoloyee.team ?? empoloyee.leadingTeam ?? null;
 
-  if (team) {
-    team.roleInTeam = empoloyee.team ? 'member' : 'leader';
-  }
-
   return {
     ...empoloyee,
-    teamId: empoloyee.teamId ?? empoloyee.leadingTeam?.id ?? null,
-    team: empoloyee.team ?? empoloyee.leadingTeam ?? null,
+    teamId: team?.id ?? null,
+    team,
     leadingTeam: undefined,
   };
 };
